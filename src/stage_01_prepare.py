a469a5389f1d48ded8a4dc3a414c4a2016976412
arjunkumbakkara <arjunkumbakkara@gmail.com>
2021-12-08 21:42:24 +0530

    Changes

---
+++ src/stage_01_prepare.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+
+
+STAGE = "Preparation " ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path, params_path):
+    ## read config files : Converting xml data to tsv
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    
+    source_data= config["source_data"]
+    input_data = os.path.join(source_data["data_dir"], source_data["data_file"])
+    split =params["prepare"]["split"]
+    seed = params["prepare"]["seed"]
+
+     
+
+    random.seed(seed)
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+    create_directories([prepared_data_dir_path])
+
+    train_data_path =os.path.join(prepared_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path =os.path.join(prepared_data_dir_path,artifacts["TEST_DATA"])
+
+    
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params) 
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- src/utils/common.py
+++ src/utils/common.py
@@ -5,7 +5,7 @@ import time
 import pandas as pd
 import json
 
-def read_yaml(path_to_yaml: str) -> dict:
+def read_yaml(path_to_yaml: str) -> dict: 
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     logging.info(f"yaml file: {path_to_yaml} loaded successfully")
